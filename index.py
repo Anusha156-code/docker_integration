4e2c03dee3a7ca7fd25b12cb724bb8c33e1a5c7d
Anusha <anusha4615@gmail.com>
2026-04-16 20:06:42 +0530

    testing webhook via jenkins adding new words

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "learning concepts is easy in devops but applying,debuging is what a real task is !!!!!! "
+    return "learning concepts is easy in devops but applying,debuging is what a real task is !!!!!! and its a task to become better every day "
 if __name__ == "__main__":
     app.run(host = "0.0.0.0" ,port= 5000)
