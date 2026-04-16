2beed6e0e7b7d3e3331ba6772fe45843a8b60f80
Anusha <anusha4615@gmail.com>
2026-04-16 19:35:12 +0530

    Intial commit

---
+++ index.py
@@ -0,0 +1,7 @@
+from flask import Flask 
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "learning concepts is easy in devops but applying,debuging is what a real task is "
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0" ,port= 5000)
