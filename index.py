af54d900fb4ed59de30d5c099b037d94412bb592
Anusha <anusha4615@gmail.com>
2026-04-17 16:21:21 +0530

    added new words

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "learning concepts is easy   in devops but applying,debuging is what a real task is !!!!!! and its a task to become better every day with consistency "
+    return "learning concepts is easy   in devops but applying,debuging is what a real task is !!!!!! and its a task to become better every day with consistency and dedication"
 if __name__ == "__main__":
     app.run(host = "0.0.0.0" ,port= 5000)
